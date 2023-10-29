04dcfd8f0c693450acbf9897149c12ef27dbd693
Camila Dias <camila.dias1127@gmail.com>
2023-10-30 08:31:32 +1100

    novo-modulo

---
+++ screenmatch-week8/src/Fillme.java
@@ -0,0 +1,16 @@
+
+public class Fillme {
+	String nome;
+	int anoDeLancamento;
+	boolean incluidoNoPlano;
+	double avaliacao;
+	int totalDeAvaliacao;
+	int duracaoEmMinutos;
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
